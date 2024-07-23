f268d0fa45b62acb0c14f3f6ba60a141b0175b06
Simonedambra <simonedambra130102@gmail.com>
2024-07-23 18:46:52 +0200

    upd stampa dell'errore

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -7,15 +7,24 @@
             <form action="{{ route('comics.store') }}" method="POST">
                 @csrf
                 {{-- NOME --}}
+
                 <div class="mb-3">
                     <label for="title" class="form-label">titolo:</label>
-                    <input type="text" class="form-control" id="title" name="title">
+                    <input type="text" class="form-control" id="title" name="title"value="{{ old('title') }}"
+                        class="@error('title') is-invalid @enderror">
                 </div>
+                @error('title')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
                 {{-- INGREDIENTI --}}
                 <div class="mb-3">
                     <label for="description" class="form-label">description:</label>
-                    <input type="text" class="form-control" id="description" name="description">
+                    <input type="text" class="form-control" id="description" name="description"
+                        value="{{ old('description') }}" class="@error('description') is-invalid @enderror">
                 </div>
+                @error('description')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
 
                 <button type="submit" class="btn btn-success">Crea nuovo Fumetto</button>
             </form>
